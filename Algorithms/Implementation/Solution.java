84ff5338d54b42394a80ac764cff633f43bff28e
rohith28 <rohithuppala28@gmail.com>
2017-08-13 21:34:38 -0400

    Grading Student problem

---
+++ Algorithms/Implementation/Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int[] grades){
+        // Complete this function
+        for(int i=0;i<grades.length;i++)
+            {
+        int multiple = (grades[i]/5) + 1;
+        int result = multiple*5;
+        if(result-grades[i] < 3)
+            {
+                if(result>39)
+                    {
+                    grades[i]=result;
+                }
+        }
+        }
+        return grades;
+        
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] grades = new int[n];
+        for(int grades_i=0; grades_i < n; grades_i++){
+            grades[grades_i] = in.nextInt();
+        }
+        int[] result = solve(grades);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+        }
+        System.out.println("");
+        
+
+    }
+}
